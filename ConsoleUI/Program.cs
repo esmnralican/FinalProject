4878e89ad345af3c806cc351dcb99003b14caec7
esmnr <esmnr@LAPTOP-AC3V0U9T>
2021-02-03 23:47:18 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    //SOLİD Prensipleri 
+    //Open CLosed Principle, yeni bir özellik ekliyorsan mevcuttaki hiçbir koda dokunmazsın.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetAllByCategoryId(2))//GetAllByUnitPrice(20,100) : 40£ ile 100£ arasındaki
+            {
+                Console.WriteLine(product.ProductName);
+            }
+            
+            
+        }
+    }
+}
