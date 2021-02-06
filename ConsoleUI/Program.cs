887274517ebbb9aaf441b959c1ad7268f37a8191
esmnr <esmnr@LAPTOP-AC3V0U9T>
2021-02-07 00:07:58 +0300

    6.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,15 +11,26 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //DTOs : Data Transformation Object
+            ProductTest();
+            // IoC
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
 
+            }
+
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAllByCategoryId(2))//GetAllByUnitPrice(20,100) : 40£ ile 100£ arasındaki
+            foreach (var product in productManager.GetProductDetails())//GetAllByUnitPrice(20,100) : 40£ ile 100£ arasındaki
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             }
-            
-            
         }
     }
 }
---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+//Core katmanı bizim evrensel katmanımız. Yani bütün .Net projelerinde kullanılabilir.Core katmanı diğer katmanları referans almamalı
+
+namespace Core.DataAccess
+{
+   public interface IEntityRepository<T> where T:class, IEntity,new()
+    {
+        //generic constraint
+        //class : referans tip
+        //IEntity : IEntity 
+        List<T> GetAll(Expression<Func<T,bool>> filter=null); 
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        //List<T> GetAllByCategory(int categoryId); asla ihtiyacımız yok. 10 ve 11. satır ile oldu
+    }
+}
