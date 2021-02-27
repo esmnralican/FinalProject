082e16ff0849e920d4eb4109fc57397d62450809
esmnr <esmnr@LAPTOP-AC3V0U9T>
2021-02-27 19:54:25 +0300

    17.02.2021

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -15,10 +15,11 @@ namespace Business.Abstract
         IDataResult<List<ProductDetailDto>> GetProductDetails();
         IDataResult<Product> GetById(int productId);
         IResult Add(Product product);
-  
-        
+        IResult Update(Product product);
+
+
         //RESTFUL--> HTTP-->
-    
+
     }
 }
 
--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -13,6 +13,7 @@ using Business.ValidationRules.FluentValidation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Aspects.Autofac.Validation;
 using Business.CCS;
+using System.Linq;
 
 namespace Business.Concrete
 {
@@ -26,16 +27,36 @@ namespace Business.Concrete
             
         }
 
-       [ValidationAspect(typeof(ProductValidator))]
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
             //business codes
             //validation
 
-               _productDal.Add(product);
+            //Aynı isimde ürün eklenemez
+            if (CheckIfProductCountOfCategoryCorrect(product.CategoryId).Success)
+            {
+                if (CheckIfProductNameExists(product.ProductName).Success)
+                {
+                    _productDal.Add(product);
+
+                    return new SuccessResult(Messages.ProductAdded);
+                }
+
+            }
+            return new ErrorResult();
+
+
+
+            if (CheckIfProductCountOfCategoryCorrect(product.CategoryId).Success)
+            {
+                _productDal.Add(product);
 
                 return new SuccessResult(Messages.ProductAdded);
-           
+
+            }
+            return new ErrorResult();
+
         }
 
         public IDataResult<List<Product>> GetAll()
@@ -72,6 +93,38 @@ namespace Business.Concrete
                 return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
             }
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
+        }
+        [ValidationAspect(typeof(ProductValidator))]
+
+        public IResult Update(Product product)
+        {
+            var result = _productDal.GetAll(p => p.CategoryId == product.CategoryId).Count;
+            if (result >= 10)
+            {
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            }
+            throw new NotImplementedException();
+        }
+
+        private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
+        {
+            //Select count(*) from products where categoryId=1
+            var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
+            if (result >= 15)
+            {
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfProductNameExists(string productName)
+        {
+            var result = _productDal.GetAll(p => p.ProductName == productName).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.ProductNameAlreadyExist);
+            }
+            return new SuccessResult();
         }
     }
 }
